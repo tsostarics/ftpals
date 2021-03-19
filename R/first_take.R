e0a54b6b1ca8a6bed888eff9a026bf1fa87c9ab9
Thomas Sostarics <tsostarics@gmail.com>
2021-03-18 23:43:48 -0500

    re export first_take()

--- R/first_take.R
+++ R/first_take.R
@@ -5,7 +5,14 @@
 #'
 #' @param index Which song to listen to (index in the palette table)
 #' @param takes Palette table
+#' @export
 first_take <- function(index = sample(seq_len(nrow(takes)), 1), takes=ftpals::first_takes){
-  message(paste0("Index: ", index,". Enjoy the music!"))
+  cat(paste0("Index: ",
+                 index,
+                 ". You're listening to ",
+                 takes[['song']][[index]],
+                 " by ",
+                 takes[['artist']][[index]],
+                 ". Enjoy the music!"))
   utils::browseURL(paste0("https://www.youtube.com/watch?v=", takes[["videoId"]][[index]]))
 }
